4a2431fac841b1d64f34f717d9c902883daf3258
JShilts <jarrod.shilts@gmail.com>
2017-05-17 18:02:41 -0500

    added GPL10558 to supported platforms

--- R/metapredict_process.R
+++ R/metapredict_process.R
@@ -129,7 +129,7 @@ getSupportedPlatforms = function() {
 	return(c('GPL180', 'GPL341', 'GPL571', 'GPL885', 'GPL887', 'GPL890', 'GPL962', 'GPL1053', 'GPL1073',
 				'GPL1291', 'GPL1293', 'GPL1390', 'GPL1708', 'GPL3921', 'GPL5645', 'GPL6254', 'GPL6333',
 				'GPL6480', 'GPL6865', 'GPL6880', 'GPL6884', 'GPL6885', 'GPL6887', 'GPL6947', 'GPL7015',
-				'GPL7202', 'GPL8177', 'GPL10332', 'GPL10379', 'GPL10687', 'GPL13607', 'GPL15331', 'GPL15450',
+				'GPL7202', 'GPL8177', 'GPL10332', 'GPL10379', 'GPL10558', 'GPL10687', 'GPL13607', 'GPL15331', 'GPL15450',
 				'GPL18721', 'GPL20769'))}
 
 
@@ -279,6 +279,8 @@ getStudyData = function(parentFolderPath, studyName, studyDataType, platformInfo
 			mapping = getGeneProbeMappingAnno(featureDf, dbName='org.Hs.egREFSEQ2EG', interName='GB_ACC')
 		} else if (platformInfo=='GPL10379') {
 			mapping = getGeneProbeMappingDirect(featureDf, geneColname='EntrezGeneID')
+		} else if (platformInfo=='GPL10558') {
+			mapping = getGeneProbeMappingDirect(featureDf, geneColname='Entrez_Gene_ID')
 		} else if (platformInfo=='GPL10687') {
 			mapping = getGeneProbeMappingDirect(featureDf, geneColname='EntrezGeneID')
 		} else if (platformInfo=='GPL13607') {
