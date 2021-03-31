b3a5e3673ee85d2b79e43d724d5e7e5c6e4f00ff
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2021-03-31 12:48:34 -0500

    Push updated data, files, and tests

--- tests/testthat/test_metapredict_predict.R
+++ tests/testthat/test_metapredict_predict.R
@@ -1,7 +1,11 @@
 library(data.table)
 parentFolderPath = 'test_data'
 foldidColname = 'study'
+className = 'class'
+family = 'multinomial'
+alpha = 0.9
 discoveryStudyNames = c('Bhattacharjee', 'GSE11969', 'GSE29016')
+classLevels = c('AD', 'SQ', 'SCLC')
 
 studyMetadataPath = file.path(parentFolderPath, 'study_metadata.csv')
 studyMetadata = read.csv(studyMetadataPath, stringsAsFactors = FALSE)
@@ -9,10 +13,22 @@ studyMetadata = read.csv(studyMetadataPath, stringsAsFactors = FALSE)
 sampleMetadataPath = file.path(parentFolderPath, 'sample_metadata.csv')
 sampleMetadata = read.csv(sampleMetadataPath, stringsAsFactors = FALSE)
 
+ematDiscoveryControl = readRDS(file.path(parentFolderPath, 'ematDiscovery.rds'))
+
 glmnetArgsControl = readRDS(file.path(parentFolderPath, 'glmnetArgs.rds'))
+
+cvFitListControl = readRDS(file.path(parentFolderPath, 'cvFitList.rds'))
 
 test_that('makeGlmnetArgs', {
   glmnetArgsTest = makeGlmnetArgs(sampleMetadata[sampleMetadata$study %in% discoveryStudyNames,],
                                   foldidColname = foldidColname)
   expect_true(all.equal(glmnetArgsTest, glmnetArgsControl, check.attributes = FALSE))
+})
+
+test_that('metapredictCv', {
+  cvFitListTest = metapredictCv(ematDiscoveryControl, sampleMetadata, yName = className,
+                                weights = glmnetArgsControl$weights, foldid = glmnetArgsControl$foldid,
+                                alpha = alpha, family = family, lambda.min.ratio = 0.001,
+                                keep = TRUE)
+  expect_true(all.equal(cvFitListTest, cvFitListControl, check.attributes = FALSE))
 })
